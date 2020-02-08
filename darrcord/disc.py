bb7b1c2fadaea1cf89f903340690334027c58e04
Christopher Eby <kreed@kreed.org>
2020-02-08 03:49:32 +0000

    Add search command; rework command handling
    Removes most of the logic from disc.py and moves each command
    into its own module. disc.py now passes messages to each command
    module, one at a time, until a command generates a reply.
    
    Removes the distinction between sonarr and radarr commands. I wanted
    to unify them in preparation for DM support.

--- darrcord/disc.py
+++ darrcord/disc.py
@@ -1,174 +1,67 @@
+import asyncio
 import discord
+from collections import defaultdict
 from config import Config
 from darrcord import sonarr
 from darrcord import radarr
 from darrcord import logger
+from darrcord.command import request, search
 
 client = discord.Client()
 
+commands = request, search
 
 def get_client():
     return client
 
+async def send_reply(cmd, channel, reply):
+    reply = defaultdict(lambda: None, reply)
+    msg = await channel.send(
+            content=reply["content"],
+            embed=reply["embed"],
+            nonce=cmd.nonce)
+    if reply["reactions"]:
+        # these should run in parallel but adding reactions is still super slow so idk
+        await asyncio.gather(*[ msg.add_reaction(emoji) for emoji in reply["reactions"] ])
 
-def parse_message(message):
-    logger.info(f"parsing message: {message}")
-    ret_obj = {}
+@client.event
+async def on_reaction_add(reaction, user):
+    """ When one of our messages is reacted to, this sends the reaction to the command that
+    generated the message (identified by the message nonce). """
 
-    son_ret = None
-    if Config.SONARR_ENABLED:
-        if str(message.channel) in Config.SONARR_CHANNELS:
-            son_ret = parse_sonarr(message)
-
-    if son_ret:
-        ret_obj["sonarr"] = True
-        ret_obj["sonarr_obj"] = son_ret
-    else:
-        ret_obj["sonarr"] = False
-
-    rad_ret = None
-    if Config.RADARR_ENABLED:
-        if str(message.channel) in Config.RADARR_CHANNELS:
-            rad_ret = parse_radarr(message)
-
-    if rad_ret:
-        ret_obj["radarr"] = True
-        ret_obj["radarr_obj"] = rad_ret
-    else:
-        ret_obj["radarr"] = False
-
-    return ret_obj
-
-
-def parse_sonarr(message):
-    ret = None
     try:
-        # commenting this out until we have more than one command.
-        # command = message.content[0].lower()
-        command = 'r'
-        func = sonarr_switch(command)
-        # commenting this out until we have more than one command.
-        # ret = func(message.content[1:].strip())
-        ret = func(message.content.strip())
-    except:
-        return None
-    return ret
+        # ignore reactions we send, reactions not on our messages
+        if user == client.user or not reaction.message.author == client.user:
+            return
 
+        logger.debug(f"Received reaction: {reaction} from {user} on {reaction.message.channel}")
 
-def sonarr_switch(command):
-    switch = {
-        'r': request_sonarr_series
-    }
-    ret = switch.get(command, null_func)
-    return ret
-
-
-def request_sonarr_series(series):
-    try:
-        id = int(series)
-        son_resp = sonarr.req_series_request(id)
-        code = son_resp["code"]
-        resp = son_resp["json"]
-        if code >= 200 and code < 400:
-            if resp:
-                ret = {
-                    'message': f"Successfully requested {resp[0]['title']}: https://www.thetvdb.com/?tab=series&id={id}",
-                    'resp': resp}
-            else:
-                ret = {'message': "Unknown error.  No error message.  Sorry."}
-        else:
-            if resp:
-                ret = {'message': f"Error adding series.  Error message is: {resp[0]['errorMessage']}.", 'resp': resp}
-            else:
-                ret = {'message': "Unknown error.  No error message.  Sorry."}
-        return ret
-    except ValueError as err:
-        logger.error(err)
-        return None
-    except Exception as e:
+        for cmd in commands:
+            if cmd.nonce == reaction.message.nonce:
+                reply = cmd.handle_reaction(reaction, user)
+                if reply:
+                    await send_reply(cmd, reaction.message.channel, reply)
+    except (ValueError, Exception) as e:
         logger.exception(e)
-        return None
-
-
-def parse_radarr(message):
-    ret = None
-    try:
-        #commenting until we have more commands
-        #command = message.content[0].lower()
-        command = 'r'
-        func = radarr_switch(command)
-        #commenting until we have more commands
-        #ret = func(message.content[1:].strip())
-        ret = func(message.content.strip())
-    except:
-        return None
-    return ret
-
-
-def radarr_switch(command):
-    switch = {
-        'r': request_radarr_movie
-    }
-    ret = switch.get(command, null_func)
-    return ret
-
-
-def request_radarr_movie(movie):
-    try:
-        id = int(movie)
-        son_resp = radarr.req_movie_request(id)
-        code = son_resp["code"]
-        resp = son_resp["json"]
-        if code >= 200 and code < 400:
-            if resp:
-                ret = {'message': f"Successfully requested {resp[0]['title']}: https://www.themoviedb.org/movie/{id}",
-                       'resp': resp}
-            else:
-                ret = {'message': "Unknown error.  No error message.  Sorry."}
-        else:
-            if resp:
-                ret = {'message': f"Error adding movie.  Error message is: {resp[0]['errorMessage']}.", 'resp': resp}
-            else:
-                ret = {'message': "Unknown error.  No error message.  Sorry."}
-        return ret
-    except ValueError as err:
-        logger.error(err)
-        return None
-    except Exception as e:
-        logger.exception(e)
-        return None
-
-
-def null_func(args):
-    return {}
-
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to look at its own messages
-    if message.author == client.user:
-        return
-    logger.debug(f"Received message: {message.content}")
-    if str(message.channel) not in Config.SONARR_CHANNELS and str(message.channel) not in Config.RADARR_CHANNELS:
-        return
+    """ This sends the message to each command until it finds one that generates a reply. """
 
-    msg = None
-
-    message_data = parse_message(message)
-    if not message_data["sonarr"] and not message_data["radarr"]:
-        if (Config.SONARR_ENABLED and str(message.channel) in Config.SONARR_CHANNELS) or (
-                Config.RADARR_ENABLED and str(message.channel) in Config.RADARR_CHANNELS):
-            msg = f"Sorry, {message.author.mention}, I didn't understand that request."
-        else:
+    try:
+        # we do not want the bot to look at its own messages
+        if message.author == client.user:
             return
-    if message_data["sonarr"]:
-        msg = f"{message.author.mention}: {message_data['sonarr_obj']['message']}"
-        await message.channel.send(msg)
-        msg = None
-    if message_data["radarr"]:
-        msg = f"{message.author.mention}: {message_data['radarr_obj']['message']}"
-        await message.channel.send(msg)
-        msg = None
 
-    if msg:
-        await message.channel.send(msg)
+        text = message.content.strip()
+        logger.debug(f"Received message: {text} on {message.channel}")
+
+        reply = None
+
+        for cmd in commands:
+            reply = cmd.handle_message(text, message)
+            if reply:
+                await send_reply(cmd, message.channel, reply)
+                break
+    except (ValueError, Exception) as e:
+        logger.exception(e)
